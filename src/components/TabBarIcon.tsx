70a2d17aa7ed1b3b99aeb8f44cf2d25d5023f5f7
Yogender Singh <yogender.singh@sportsbaazi.com>
2025-03-10 00:10:42 +0530

    tabBarIcon with Unistyles

--- src/components/TabBarIcon.tsx
+++ src/components/TabBarIcon.tsx
@@ -2,12 +2,14 @@ import { StyleSheet } from "react-native"
 
 import { Icon } from "./Icon"
 
-export const TabBarIcon = (props: {
+import { withUnistyles } from "react-native-unistyles"
+
+export const TabBarIcon = withUnistyles(function TabBarIcon(props: {
   icon: React.ComponentProps<typeof Icon>["icon"]
   color: string
-}) => {
+}) {
   return <Icon size={28} style={styles.tabBarIcon} {...props} />
-}
+})
 
 export const styles = StyleSheet.create({
   tabBarIcon: {
