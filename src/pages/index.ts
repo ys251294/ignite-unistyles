b5e3593d93adcaeb774db20ef6247b70a9eb203c
Yogender Singh <yogender.singh@sportsbaazi.com>
2025-03-15 19:04:44 +0530

    removing routing code

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,2 @@
 export { default as NotFoundPage } from "./not-found/NotFoundPage"
 export { default as WelcomePage } from "./welcome/WelcomePage"
-export * from "./login/LoginPage"
-export * from "./home/HomePage"
